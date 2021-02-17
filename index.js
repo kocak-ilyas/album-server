fed9aa925898c42ba99bdb59dee576416262dd86
kocak-ilyas <62467305+kocak-ilyas@users.noreply.github.com>
2021-02-17 09:44:56 +0300

    port

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
+// const PORT = 4000 || 5000;
+// const PORT = process.env.server_port || 5000;
+
 //
 app.get("/", (req, res) => {
   res.send("Server API is Running");
@@ -21,8 +24,7 @@ app.get("/", (req, res) => {
 const CONNECTION_URL = `mongodb+srv://album:wZ7JJ98D0SDi8jVV@cluster0.tk2q6.mongodb.net/albumDB?retryWrites=true&w=majority`;
 // const CONNECTION_URL = `mongodb+srv://${process.env.mongoDB_user}:${process.env.mongoDB_key}@cluster0.tk2q6.mongodb.net/${process.env.mongoDB_dataBase}?retryWrites=true&w=majority`;
 
-const PORT = 4000 || 5000;
-// const PORT = process.env.server_port || 5000;
+
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
