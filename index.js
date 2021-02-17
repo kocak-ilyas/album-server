d84cc4d8326179f1a523737fce147e29d00e0f23
kocak-ilyas <62467305+kocak-ilyas@users.noreply.github.com>
2021-02-17 16:33:09 +0300

    back

--- index.js
+++ index.js
@@ -14,9 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = `mongodb+srv://album:wZ7JJ98D0SDi8jVV@cluster0.tk2q6.mongodb.net/albumDB?retryWrites=true&w=majority`;
-
-// const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const CONNECTION_URL = 'mongodb+srv://album:wZ7JJ98D0SDi8jVV@cluster0.tk2q6.mongodb.net/test';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
