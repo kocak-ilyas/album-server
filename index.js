751de12ce3e005a6e221bc4740ee6ddb51276e29
kocak-ilyas <62467305+kocak-ilyas@users.noreply.github.com>
2021-02-17 19:57:24 +0300

    url

--- index.js
+++ index.js
@@ -14,7 +14,18 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://album:wZ7JJ98D0SDi8jVV@cluster0.tk2q6.mongodb.net/test';
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://album:album456@cluster0.tk2q6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+const CONNECTION_URL = 
+"mongodb+srv://album:album456@cluster0.tk2q6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// 'mongodb+srv://album:wZ7JJ98D0SDi8jVV@cluster0.tk2q6.mongodb.net/test';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
