abdeb644d991ea7510c5b32634f2f071df1d7149
kocak-ilyas <62467305+kocak-ilyas@users.noreply.github.com>
2021-02-17 16:25:32 +0300

    last

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-// import dotenv from "dotenv";
-import postRoutes from "./routes/posts.js";
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
 
 const app = express();
-// dotenv.config();
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use("/posts", postRoutes);
+app.use('/posts', postRoutes);
 
-// const PORT = 4000 || 5000;
-// const PORT = process.env.server_port || 5000;
-
-//
-app.get("/", (req, res) => {
-  res.send("Server API is Running");
-});
-//
 const CONNECTION_URL = `mongodb+srv://album:wZ7JJ98D0SDi8jVV@cluster0.tk2q6.mongodb.net/albumDB?retryWrites=true&w=majority`;
-// const CONNECTION_URL = `mongodb+srv://${process.env.mongoDB_user}:${process.env.mongoDB_key}@cluster0.tk2q6.mongodb.net/${process.env.mongoDB_dataBase}?retryWrites=true&w=majority`;
 
+// const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const PORT = process.env.PORT|| 5000;
 
-mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set("useFindAndModify", false);
+mongoose.set('useFindAndModify', false);
