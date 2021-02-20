857aad363d749c604c6c26350ed0b896c8080ab4
kocak-ilyas <62467305+kocak-ilyas@users.noreply.github.com>
2021-02-20 12:13:18 +0300

    pasckage.json

--- index.js
+++ index.js
@@ -6,24 +6,16 @@ import cors from "cors";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  `mongodb+srv://${process.env.USER}:${process.env.KEY}@cluster0.tk2q6.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`
-  // "mongodb+srv://album:album456@cluster0.tk2q6.mongodb.net/albumDB?retryWrites=true&w=majority";
-
+const CONNECTION_URL = `mongodb+srv://${process.env.USER}:${process.env.KEY}@cluster0.tk2q6.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`))
-  )
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
   .catch((error) => console.log(`Connection Error: ${error}`));
-
 mongoose.set("useFindAndModify", false);
