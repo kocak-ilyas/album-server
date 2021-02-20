7893b1084bdc61eb283393f6999cacbdaa05ade3
kocak-ilyas <62467305+kocak-ilyas@users.noreply.github.com>
2021-02-20 11:36:03 +0300

    index.js

--- index.js
+++ index.js
@@ -14,22 +14,13 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://album:album456@cluster0.tk2q6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
 const CONNECTION_URL = 
 "mongodb+srv://album:album456@cluster0.tk2q6.mongodb.net/albumDB?retryWrites=true&w=majority";
-// 'mongodb+srv://album:wZ7JJ98D0SDi8jVV@cluster0.tk2q6.mongodb.net/test';
-const PORT = process.env.PORT|| 5000;
+
+// const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  // .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
