7de0576799fc7892d1ebfc9d6f82dccadfe60359
kocak-ilyas <62467305+kocak-ilyas@users.noreply.github.com>
2021-02-20 11:45:12 +0300

    const port

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-
-import postRoutes from './routes/posts.js';
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
+app.use("/posts", postRoutes);
 
-const CONNECTION_URL = 
-"mongodb+srv://album:album456@cluster0.tk2q6.mongodb.net/albumDB?retryWrites=true&w=majority";
+const CONNECTION_URL =
+  "mongodb+srv://album:album456@cluster0.tk2q6.mongodb.net/albumDB?retryWrites=true&w=majority";
 
-// const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
   .catch((error) => console.log(`Connection Error: ${error}`));
 
-mongoose.set('useFindAndModify', false);
+mongoose.set("useFindAndModify", false);
