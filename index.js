b57c5d8ea1b15359a2b3430ae2d919171dd6c879
kocak-ilyas <62467305+kocak-ilyas@users.noreply.github.com>
2021-02-20 18:52:09 +0300

    start to auth

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
+dotenv.config();
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -16,6 +17,8 @@ const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`))
+  )
   .catch((error) => console.log(`Connection Error: ${error}`));
 mongoose.set("useFindAndModify", false);
