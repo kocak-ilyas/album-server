ebd14ec256de162485d181c3660f1f1394453fb3
kocak-ilyas <62467305+kocak-ilyas@users.noreply.github.com>
2021-03-23 12:22:43 +0300

    port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use("/posts", postRoutes);
 
 const CONNECTION_URL = `mongodb+srv://${process.env.USER}:${process.env.KEY}@cluster0.tk2q6.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`;
-const PORT = 4000 // process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
