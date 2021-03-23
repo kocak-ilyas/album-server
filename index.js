1b7e92baf00a02f2233d5b1a3a4dba4dc9acc388
kocak-ilyas <62467305+kocak-ilyas@users.noreply.github.com>
2021-03-23 17:24:14 +0300

    jwt token start

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
 
 const CONNECTION_URL = `mongodb+srv://${process.env.USER}:${process.env.KEY}@cluster0.tk2q6.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
